1ec2578afe7257a13defe69e9980e770173e7508
Joern Hees <dev@joernhees.de>
2016-12-20 14:45:59 +0100

    noinspect six.moves

--- splendid/__init__.py
+++ splendid/__init__.py
@@ -4,6 +4,7 @@ from __future__ import division
 from __future__ import print_function
 
 from functools import wraps
+# noinspection PyUnresolvedReferences
 from six.moves import zip_longest
 from timeit import default_timer as timer
 import datetime
